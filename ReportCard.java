99b1190ac2d1ba8731eb42ad7cf671df53f821b0
rootikigai <ifykel360@gmail.com>
2025-04-26 16:08:57 +0100

    Modifications made to handle count for valid and invalid inputs

--- ReportCard.java
+++ ReportCard.java
@@ -12,18 +12,13 @@ public class ReportCard{
     int countC = 0;
     int countD = 0;
     int countF = 0;
-    int countScores = 0;
 
+    for(count = 1; count <= 10; count++){
     System.out.println("Enter your valid score: ");
     int score = input.nextInt();
-    
-    for(count = 1; count <= 10; count++){
 
-      if(score < 0 || score > 100){
-        System.out.print("Invalid input. Try again");
-        countInv++;
-      }
-      else{
+      if(score >= 0 && score <= 100){
+        countVal++;
 
 /*
 80-100 A
@@ -39,39 +34,38 @@ public class ReportCard{
                       score = input.nextInt();
                       countA++;
                       countVal++;
-                      countScores =+ countA;
-            break;
+                      break;
           case 7: System.out.println("B");
             case 12: System.out.print("Enter another score: ");
                       score = input.nextInt();
                       countB++;
                       countVal++;
-                      countScores =+ countB;
                       break;
           case 6: System.out.println("C");
             case 13: System.out.print("Enter another score: ");
                       score = input.nextInt();
                       countC++;
                       countVal++;
-                      countScores =+ countC;
                       break;
           case 5: System.out.println("D");
             case 14: System.out.print("Enter another score: ");
                       score = input.nextInt();
                       countD++;
                       countVal++;
-                      countScores =+ countD;
                       break;
           case 4, 3, 2, 1: System.out.println("F");
             case 15: System.out.print("Enter another score: ");
                       score = input.nextInt();
                       countF++;
                       countVal++;
-                      countScores =+ countF;
                       break;
         }
       }
+      else {
+        System.out.println("Invalid input!");
+        countInv++;
+      }
     }
-    System.out.printf("Invalid inputs: %d%nValid inputs: %d%nScore count Grade A: %d%nScore count Grade B: %d%nScore count Grade C: %d%nScore count Grade D: %d%nScore count Grade F: %d%n", countInv, countVal, countA, countB, countC, countD, countF);
+    System.out.printf("%nInvalid inputs: %d%nValid inputs: %d%nScore count Grade A: %d%nScore count Grade B: %d%nScore count Grade C: %d%nScore count Grade D: %d%nScore count Grade F: %d%n", countInv, countVal, countA, countB, countC, countD, countF);
   }
 }
