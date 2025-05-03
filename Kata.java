2663b83759949f1f1d35b7578f1e0f5e4e980f6a
rootikigai <ifykel360@gmail.com>
2025-05-03 14:28:20 +0100

    Wrote boolean function that returns true or false if number is prime or not

--- Kata.java
+++ Kata.java
@@ -3,4 +3,17 @@ public class Kata{
   public static boolean isEven(int num){
     return num % 2 == 0;
   }
+
+  public static boolean isPrimeNum(int num){
+      for(int i = 2;i * i <= num; i++){
+        if(num % i == 0){
+          return false; //why stress typing !true...not stress though. You'd still tap the keyboard 5 times for each of 'em. No!...6 times for !true, 'cause of the shift key and 1 to get '!'. Mtcheew...ignore my thoughts!!!
+        }
+      }
+      return true;
+  }
+
+  /*public static int posDiff(){
+
+  }*/
 }
